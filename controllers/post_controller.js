9f011ac4f8acaf863aa519f5d1c62110a9f95bfc
Parth <parth043164@gmai.com>
2022-09-25 12:49:19 +0530

    Delete a comment of user

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post')
+const Comment=require('../models/comment')
 
 
 module.exports.postContent=function(req,res){
@@ -9,5 +10,24 @@ module.exports.postContent=function(req,res){
         if(err){console.log('error in finding the Post ');return;}
 
         return res.redirect('back');
+    })
+}
+module.exports.destroy=function(req,res){
+    //finding the id , by the url
+    Post.findById(req.params.id,function(err,post){
+
+        //checking the correct person
+        //in req.user.id instead of _id , we use .id , bcoz it is converting into string
+        //as we require string
+
+        if(post.user==req.user.id){
+
+            post.remove();
+            Comment.deleteMany({post:req.param.id},function(err){
+             return res.redirect('back');   
+            })
+        }else{
+            return res.redirect('back'); 
+        }
     })
 }
--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,8 @@ const passport=require('passport');
 const PostController=require('../controllers/post_controller')
 
 router.post('/create',passport.checkAuthentication,PostController.postContent)
+router.get('/destroy/:id',passport.checkAuthentication,PostController.destroy)
+
 
 module.exports=router
 
